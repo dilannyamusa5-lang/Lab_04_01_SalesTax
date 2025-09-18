f2fe00c70d2316b84d3c55ba2e81acb4841e652d
Nyamusa <nyamuspa@mail.uc.edu>
2025-09-18 13:13:09 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+     double itemprice = 9.99;
+     final double TAX_RATE = 0.05;
+
+     double totalPrice = itemPrice + (itemPrice * TAX_RATE);
+     double taxAmount = itemPrice * Tax_rate;
+
+     System.out.println("Total Price: $" + totalPrice);
+     System.out.println("Tax Amount: $" + TaxAmount);
+        }
+    }
+}
