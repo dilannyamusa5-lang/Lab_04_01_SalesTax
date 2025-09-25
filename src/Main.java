38a31ab311cebed0642af603149ce682f1c15dba
dilannyamusa5-lang <dilannyamusa5@gmail.com>
2025-09-24 23:08:22 -0400

    complete

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-     double itemprice = 9.99;
+     double itemPrice = 9.99;
      final double TAX_RATE = 0.05;
 
      double totalPrice = itemPrice + (itemPrice * TAX_RATE);
-     double taxAmount = itemPrice * Tax_rate;
+     double taxAmount = itemPrice * TAX_RATE;
 
      System.out.println("Total Price: $" + totalPrice);
-     System.out.println("Tax Amount: $" + TaxAmount);
+     System.out.println("Tax Amount: $" +taxAmount);
         }
     }
-}
